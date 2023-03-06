d773eec989087014e1b9900c08c77253768fbee7
Yidnekachew Kassahun Kibret <yamiyekn@gmail.com>
2023-03-06 16:14:51 +0300

    Add todo list app codes for code review purpose.

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import './index.css';
+import IconOne from './Icon/Cached.svg';
+import IconTwo from './Icon/add.svg';
+import addTodo from './modules/addTodo.js';
+import populateList from './modules/populateList.js';
+import displayTodos from './modules/displayTodo.js';
+import clearCompleted from './modules/clear.js';
+import { addData } from './modules/localStorage.js';
+
+// select elements and store them in a variable
+const clear = document.querySelector('.clear');
+const refreshLogo = document.querySelector('.refresh');
+refreshLogo.innerHTML = ` <img src="${IconOne}" alt="Cached logo" />`;
+const addLogo = document.querySelector('.add-logo');
+addLogo.innerHTML = ` <img src="${IconTwo}" alt="add logo" />`;
+const input = document.getElementsByTagName('input')[0];
+const add = document.querySelector('.add-logo');
+
+// creating an empty array
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
+// Event listener for adding user input values
+add.addEventListener('click', (e) => {
+  e.preventDefault();
+  const inputData = input.value.trim();
+  if (inputData !== '') {
+    addTodo(todoList);
+  }
+});
+
+// Event listener for adding user input values
+input.addEventListener('keyup', (e) => {
+  e.preventDefault();
+  const inputData = input.value.trim();
+  if (inputData !== '' && e.keyCode === 13) {
+    addTodo(todoList);
+  }
+});
+
+// clearing the completed tasks
+clear.addEventListener('click', () => {
+  todoList = clearCompleted(todoList);
+  addData(todoList);
+  populateList(displayTodos(todoList));
+});
+
+// reloading the page
+refreshLogo.addEventListener('click', () => {
+  // eslint-disable-next-line no-restricted-globals
+  location.reload();
+});
+
+// calling the function to display
+if (todoList.length) populateList(displayTodos(todoList));
